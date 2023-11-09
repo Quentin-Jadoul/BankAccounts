8241209d4459084d246bf41ad11459ac6d72d7b8
Olingo <195367@ecam.be>
2023-11-09 23:50:58 +0100

    created db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // db.js
-const mysql = require('mysql2/promise');
+const mysql = require('mysql');
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
   host: 'your_database_host',
   user: 'your_database_user',
   password: 'your_database_password',
@@ -10,5 +10,47 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
+
+// var db = mysql.createConnection({
+//   host: "localhost",
+//   user: "rpi2",
+//   password: "local",
+// });
+
+
+
+
+function executeQuery(query, callback) {
+  db.connect(function(err) {
+      if (err) throw err;
+      // console.log("Connected!");
+      db.query(query, function(err, result) {
+          if (err) throw err;
+          callback(result);
+      });
+    });
+
+}
+
+
+executeQuery("CREATE DATABASE IF NOT EXISTS notesapp;", function(result) {
+  // console.log(result);
+});
+
+db.query("USE notesapp;", function(err, result) {
+  if (err) throw err;
+  // console.log("Using SchoolApp database.");
+});
+
+db.query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255),  password VARCHAR(255),role VARCHAR(255));", function(err, result) {
+  if (err) throw err;
+  // console.log("Teachers table created successfully.");
+});
+
+db.query("CREATE TABLE IF NOT EXISTS notes (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255),  body VARCHAR(255),access VARCHAR(255));", function(err, result) {
+  if (err) throw err;
+  // console.log("Teachers table created successfully.");
+});
+
 
 module.exports = pool;
